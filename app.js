3c6b9d7ae3903679e051448ba3b664af93cdf75f
Ilya <iliakrapivkin@gmail.com>
2020-11-14 20:55:04 +0300

    pre_redux

---
+++ app.js
@@ -0,0 +1,39 @@
+const redux = require('redux');
+
+const initialState = {
+  user: {
+    name: '',
+    age: ''
+  },
+  car: {
+    brand: '',
+    model: ''
+  }
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_USER_NAME":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          name: action.payload
+        }
+      }
+      default:
+        return state;
+  }
+}
+
+const store = redux.createStore(reducer, initialState);
+
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+
+store.dispatch({
+  type: "UPDATE_USER_NAME",
+  payload: "Tapac"
+})
